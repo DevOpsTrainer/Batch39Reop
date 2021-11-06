d642aaf7a6e9b909c1fb4d75d7710e84fd494ab2
DevOps Trainer <mydevopstrainer@gmail.com>
2021-11-06 10:50:23 -0400

    modified

---
+++ dsl-automation/createViews.groovy
@@ -0,0 +1,107 @@
+println ""
+println "-------Starting Views Creation Process--------"
+println ""
+
+def srcGrp = "Applications"
+println "Main View: ${srcGrp}"
+
+Properties serviceViews = new Properties()
+              File confPropertiesFile = new File("$WORKSPACE/dsl-automation/appViews.properties")
+              confPropertiesFile.withInputStream {
+                  serviceViews.load(it)
+              }
+Properties confProp = new Properties()
+              File confPropertiesEnvFile = new File("$WORKSPACE/dsl-automation/appConf.properties")
+              confPropertiesEnvFile.withInputStream {
+                  confProp.load(it)
+              }
+
+def sViews = evaluate("${serviceViews.serView}")
+def subgroup = (sViews.get(srcGrp))
+println "Services: ${subgroup}"
+println ""
+  
+nestedView("${srcGrp}") {
+    subgroup.each { serName ->
+    println "-------Starting ${serName} Service Views--------------"
+    views {
+    nestedView(serName) {
+      
+  views {
+      listView("Build") {
+      jobs {
+        name("${serName}")
+        regex("${serName}_Builds")
+      }
+      columns {
+        status()
+	name()
+	lastSuccess()
+	lastFailure()
+	lastDuration()
+        buildButton()
+      }
+    println "Creating Build View."
+    } 
+
+ buildMonitorView("Deploy Dashboard") {
+          jobs {
+            name('bar')
+            regex("${serName}_.*_Deploy")
+          }
+          columns {
+            status()
+    	name()
+    	lastSuccess()
+    	lastFailure()
+    	lastDuration()
+            buildButton()
+          }
+     println "Creating Dashboard View."
+    }
+  nestedView("Deploys") {
+  views {
+      
+      def iibEnvvironments = "${confProp.appEnviews}"
+      def allEnv = Eval.me(iibEnvvironments)
+      
+        for (def envT : allEnv) {
+          println "Creating ${envT} Environment View."
+    listView(envT) {
+      jobs {
+        name("${serName}")
+        regex("${serName}_${envT}_.*")
+      }
+      columns {
+        status()
+	    name()
+	    lastSuccess()
+	    lastFailure()
+	    lastDuration()
+        buildButton()
+      }
+    }
+        }
+   
+  }
+      configure { view ->
+    view / defaultView('Sandbox')
+    println "Configuring Deployes default View to Sandbox ."
+      }
+    }
+
+  }
+      
+configure { view ->
+    view / defaultView('Build')
+    println "Configuring Main View defaults to Build."
+       }
+  
+     }
+    }
+      println "-------End Of ${serName} Service Views--------------"
+  } 
+  
+}
+println ""
+println "-------End Of IIB Views Creation Process--------"
